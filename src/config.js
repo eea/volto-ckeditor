76d9103360cdf94dbd81d1296458f92ab004df21
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-21 08:07:16 +0200

    Added text block

--- src/config.js
+++ src/config.js
@@ -1,14 +1,18 @@
-import CKEditorWidget from './CKEditor/Widget';
+import CKEditorWidget from './Widget/Widget';
+import TextBlockView from './Block/View';
+import TextBlockEdit from './Block/Edit';
 
 export function applyConfig(config) {
-  return {
-    ...config,
-    widgets: {
-      ...config.widgets,
-      widget: {
-        ...config.widgets.widget,
-        cktext: CKEditorWidget,
-      },
-    },
+  config.widgets.widget.cktext = CKEditorWidget;
+
+  config.blocks.blocksConfig.cktext = {
+    id: 'cktext',
+    group: 'text',
+    title: 'CKEditor',
+    view: TextBlockView,
+    edit: TextBlockEdit,
+    icon: config.blocks.blocksConfig.text.icon,
   };
+
+  return config;
 }
